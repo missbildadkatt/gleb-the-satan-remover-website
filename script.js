df955f071b1cd5163390e7476e2e064fdd274eb7
missbildadkatt <michael.durden000@gmail.com>
2024-07-03 17:39:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const buyButton = document.getElementById("buy-button");
+
+buyButton.addEventListener("click", () => {
+  alert("what? would you like to watch some gay porn? okay my baby ;)");
+  document.location.href = "https://pornhub.com/gay";
+});
