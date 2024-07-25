e3e7f7c4ef9d949e4115efb725d9ab843ad824db
missbildadkatt <michael.durden000@gmail.com>
2024-07-25 20:58:16 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-/*const buyButton = document.getElementById("buy-button");
+document.addEventListener('DOMContentLoaded', function() {
+  const labels = document.querySelector('.current-year')
+  labels.textContent = new Date().getFullYear()
 
-buyButton.addEventListener("click", () => {
-  alert("what? would you like to watch some gay porn? okay my baby ;)");
-  document.location.href = "https://pornhub.com/gay";
-});*/
+  const images = document.querySelector('.thing-images')
+  for (let i = 0; i < 6; i++) {
+    let child = document.createElement('img')
+    child.setAttribute('src', `/images/photo_${i + 1}_2024-07-25_20-01-53.jpg`)
+    child.setAttribute('width', '200px')
+    images.appendChild(child)
+  }
+})
